08af999f5ae4bc61defd984229df8edb573fe098
Ramond Julio <ramonjulio9999@gmail.com>
2022-09-26 07:02:02 +0700

    Adding Modal Compnents, update Cart.js change div to modal components, adding div id overlays on index.html file

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+//helper constant for portal calls based on id in index.html on public folder
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDom.createPortal(<Backdrop />, portalElement)}
+      {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+    </>
+  );
+};
+
+export default Modal;
