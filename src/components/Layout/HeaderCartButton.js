3334d3d6b41ddc619e59b40b99a9d53609ae209a
Ramond Julio <ramonjulio9999@gmail.com>
2022-09-30 12:17:53 +0700

    Adding CartContext to HeaderCartButton.js file

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./HeaderCartButton.module.css";
+import cartContext from "../../store/cart-context";
 import CartIcon from "../Carts/CartIcon";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(cartContext);
+  //reduce is built in method to transform array of data to a single value (number in this case)
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
