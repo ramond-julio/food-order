8f39b87cbdec5d6dbbba31b0984896888a435731
Ramond Julio <ramonjulio9999@gmail.com>
2022-10-01 07:13:37 +0700

    Adding CartItem.js and CartItem.module.css file, Adding Context on Cart.js, change list to CartItem component

--- src/components/Carts/Cart.js
+++ src/components/Carts/Cart.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../UI/Modal";
-
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
+import cartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const cartCtx = useContext(cartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          // called bind to make sure the id to be added or remove item is pass to the handler
+          //bind preconfigure function for future execution and allows to preconfigure argument that function
+          //will receivedwhen executed
+          //needed here to receive id or item for handler
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -17,13 +38,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
